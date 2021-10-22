73e7c3b5ece355d4387e556ea72e74475b45e733
danilhautilus911 <danil.matlahov@yandex.ru>
2021-10-23 03:16:23 +0700

    Update posts_controller.rb

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -64,6 +64,6 @@ class PostsController < ApplicationController
 
     # Only allow a list of trusted parameters through.
     def post_params
-      params.require(:post).permit(:name, :title, :author, :content)
+      params.require(:post).permit(:name, :title, :author, :content, :image)
     end
 end
