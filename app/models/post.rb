1f89476db4543917f50724e06a47e36ec3a24623
danilhautilus911 <danil.matlahov@yandex.ru>
2021-10-23 03:16:21 +0700

    Update post.rb

--- app/models/post.rb
+++ app/models/post.rb
@@ -3,4 +3,5 @@ class Post < ApplicationRecord
 		validates :title, :presence => true,
 											:length => { :minimum => 5 }
     has_many :comments, :dependent => :destroy
+		mount_uploader :image, ImageUploader
 end
