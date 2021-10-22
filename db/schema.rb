1682f4037c1e495aed73d2420c2c276187c959ff
danilhautilus911 <danil.matlahov@yandex.ru>
2021-10-23 03:16:06 +0700

    Update schema.rb

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_10_22_054134) do
+ActiveRecord::Schema.define(version: 2021_10_22_151441) do
 
   create_table "comments", force: :cascade do |t|
     t.string "commenter"
@@ -28,6 +28,7 @@ ActiveRecord::Schema.define(version: 2021_10_22_054134) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.string "author"
+    t.string "image"
   end
 
   add_foreign_key "comments", "posts"
