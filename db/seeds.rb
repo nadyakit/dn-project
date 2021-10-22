69726f07e17ec655e57817ccb05e6b6ae3835601
danilhautilus911 <danil.matlahov@yandex.ru>
2021-10-23 03:16:04 +0700

    Update seeds.rb

--- db/seeds.rb
+++ db/seeds.rb
@@ -3,15 +3,39 @@ puts "Destroyed everything you touch"
 
 posts = [
 {
-	name: "New Album",
-	title: "The Bad Seeds",
-	author: "Nick Cave",
+	image: File.open(Rails.root.join('public', 'images', 'pic-1.jpg')),
+	name: "Витамины",
+	title: "Откуда брать витамин С зимой?",
 	content: "Tracklist"
 },
 {
-	name: "Old Album",
-	title: "The Bad Seeds Old",
-	author: "Nick Cave Old Too",
+	image: File.open(Rails.root.join('public', 'images', 'pic-2.jpg')),
+	name: "Супер быстрый рецепт",
+	title: "Пряный латте со свежей тыквой ",
+	content: "Some Old Tracklist"
+},
+{
+	image: File.open(Rails.root.join('public', 'images', 'pic-3.jpg')),
+	name: "Нужно больше белка!",
+	title: "В каких продуктах белка больше, чем в курице?",
+	content: "Some Old Tracklist"
+},
+{
+	image: File.open(Rails.root.join('public', 'images', 'pic-4.jpg')),
+	name: "Витамины",
+	title: "Витамин А",
+	content: "Some Old Tracklist"
+},
+{
+	image: File.open(Rails.root.join('public', 'images', 'pic-5.jpg')),
+	name: "Орехи",
+	title: "Фисташки",
+	content: "Some Old Tracklist"
+},
+{
+	image: File.open(Rails.root.join('public', 'images', 'pic-6.jpg')),
+	name: "Грибы",
+	title: "Какие витамины содержатся в шампиьнонах?",
 	content: "Some Old Tracklist"
 }
 ]
