49a3fd54e16a613f9c2ee14666191d24da782458
nadyakit <nadezda.sysol2@mail.ru>
2021-10-23 04:39:47 +0300

    Update posts_controller.rb

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,4 +1,5 @@
 class PostsController < ApplicationController
+  http_basic_authenticate_with :name => "admin", :password => "ndpassword", :except => [:index, :show]
   before_action :set_post, only: %i[ show edit update destroy ]
 
   # GET /posts or /posts.json
