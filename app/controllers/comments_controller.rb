f0d06d0cd682c4218d978694affb0c4d9e7ca840
danilhautilus911 <danil.matlahov@yandex.ru>
2021-12-21 21:10:51 +0700

    Update comments_controller.rb

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,13 +1,13 @@
 class CommentsController < ApplicationController
-	http_basic_authenticate_with :name => "admin", :password => "ndpassword", :only => :destroy
+	# http_basic_authenticate_with :name => "admin", :password => "ndpassword", :only => :destroy
 	def create
 		@post = Post.find(params[:post_id])
-		@comment = @post.comments.create(params[:comment].permit(:commenter, :body))
+		@comment = @post.comments.create(params[:comment].permit(:body).merge(user_id: current_user.id))
 		redirect_to post_path(@post)
 	end
   def destroy
     @post = Post.find(params[:post_id])
-    @comment = @post.comments.find(params[:id])
+		@comment = @post.comments.create(params[:comment].permit(:body).merge(user_id: current_user.id))
     @comment.destroy
     redirect_to post_path(@post)
   end
