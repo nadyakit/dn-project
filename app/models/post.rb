a15e32431a9404010e4bc76de47cd752a370cd28
danilhautilus911 <danil.matlahov@yandex.ru>
2021-12-21 21:08:06 +0700

    Update post.rb

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,7 +1,12 @@
 class Post < ApplicationRecord
+		mount_uploader :image, ImageUploader
 		validates :name, :presence => true
 		validates :title, :presence => true,
 											:length => { :minimum => 5 }
+
     has_many :comments, :dependent => :destroy
-		mount_uploader :image, ImageUploader
+		has_many :likes, dependent: :destroy
+		has_many :favorites, dependent: :destroy
+		belongs_to :user
+		belongs_to :category
 end
