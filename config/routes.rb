6f52d991215917e24373ac56a7b0b3435eb94dca
danilhautilus911 <danil.matlahov@yandex.ru>
2021-12-21 21:09:31 +0700

    Update routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,18 @@
 Rails.application.routes.draw do
+  resources :categories
+  mount Ckeditor::Engine => '/ckeditor'
+  devise_for :users
   resources :subscribers
+
   resources :posts do
-	   resources :comments
+      resources :comments
+      resources :likes
+      resources :favorites
   end
+
+  get 'users', to: 'users#index'
+  get 'users/:id' => 'users#show', :as => :user
+  delete 'users/:id', to: 'users#destroy'
 
   get 'about/index'
   get 'promo/index'
@@ -11,7 +21,7 @@ Rails.application.routes.draw do
   get 'promo', to: 'promo#index'
   get 'about', to: 'about#index'
 
-  root 'promo#index'
+  root 'about#index'
 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
