1e651cd2ac6c16c912362fbacb66b08c7f8a4a72
danilhautilus911 <danil.matlahov@yandex.ru>
2021-12-23 00:33:28 +0700

    Update application_controller.rb

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,5 +6,9 @@ class ApplicationController < ActionController::Base
     def configure_permitted_parameters
       devise_parameter_sanitizer.permit(:sign_up, keys: [:username])
 			devise_parameter_sanitizer.permit(:account_update, keys: [:username])
+      devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
+			devise_parameter_sanitizer.permit(:account_update, keys: [:name])
+      devise_parameter_sanitizer.permit(:sign_up, keys: [:surname])
+      devise_parameter_sanitizer.permit(:account_update, keys: [:surname])
     end
 end
