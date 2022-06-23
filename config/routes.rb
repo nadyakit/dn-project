5a03652bd011a850a3f94abf9c4e342c5528c001
nadyakit <nadezda.sysol2@mail.ru>
2022-06-23 20:40:47 +0300

    Update routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -13,11 +13,14 @@ Rails.application.routes.draw do
       resources :favorites
   end
 
+  root 'about#index'
+
+
   get 'users', to: 'users#index'
   get 'users/:id' => 'users#show', :as => :user
   delete 'users/:id', to: 'users#destroy'
 
-  get 'about/index'
+  get 'about/index', to: 'about#index'
   get 'promo/index'
   get 'posts/index'
   get 'comments/index'
@@ -26,7 +29,6 @@ Rails.application.routes.draw do
   get 'promo', to: 'promo#index'
   get 'about', to: 'about#index'
 
-  root 'about#index'
 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
