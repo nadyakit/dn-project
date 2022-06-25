fa0dae7f1cd85a0c10bedca89f78e27f60b6420d
nadyakit <nadezda.sysol2@mail.ru>
2022-06-26 01:41:35 +0300

    убила свой первый компонент

--- app/javascript/components/O_header.jsx
+++ app/javascript/components/O_header.jsx
@@ -1,10 +1,12 @@
-import React from "react";
-import PropTypes from "prop-types";
+// import React from "react";
+// import PropTypes from "prop-types";
 
-const Header = () => {
-  return <div>
-    <input placeholder="Хочу найти..." className="firsttry" />
-  </div>;
-};
+// const Header = () => {
+//   return <div>
+//     <input placeholder="Хочу найти..." className="firsttry" />
+//   </div>;
+// };
 
-export default Header;
+// export default Header;
+
+// это был мой первый компонент в реакте, но я его сразу же убила
